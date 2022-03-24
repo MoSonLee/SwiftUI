773c5aada84b29dac362cf0b8dc184dde397832e
이승후 <ron222@naver.com>
2022-03-25 01:59:27 +0900

    Feat: AssetSummary Data 받아오기 구현 완료

--- MyAssets/AssetSummaryData.swift
+++ MyAssets/AssetSummaryData.swift
@@ -7,14 +7,29 @@
 
 import SwiftUI
 
-struct AssetSummaryData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class AssetSummaryData: ObservableObject {
+    @Published var assets: [Asset] = load("assets.json")
+}
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError(filename + "을 찾을 수 없습니다.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError(filename + "을 찾을 수 없습니다")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }
+    catch {
+        fatalError(filename + "을\(T.self)로 파싱할 수 없습니다.")
     }
 }
 
-struct AssetSummaryData_Previews: PreviewProvider {
-    static var previews: some View {
-        AssetSummaryData()
-    }
-}
