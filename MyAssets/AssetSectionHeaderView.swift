01090a3e7efa16f70e60b2defd6ff48d5b4447d9
이승후 <ron222@naver.com>
2022-03-25 01:59:46 +0900

    Add: AssetSectionHeaderView 추가

---
+++ MyAssets/AssetSectionHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  AssetSectionHeaderView.swift
+//  MyAssets
+//
+//  Created by 이승후 on 2022/03/25.
+//
+
+import SwiftUI
+
+struct AssetSectionHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AssetSectionHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetSectionHeaderView()
+    }
+}
