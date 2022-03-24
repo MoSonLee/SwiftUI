5d1d9a42d92f68284444e961955061d36da0e17f
이승후 <ron222@naver.com>
2022-03-24 22:25:02 +0900

    Feat: AssetView에 AssetBannerView를 띄워줌

--- MyAssets/MyAssets/AssetView.swift
+++ MyAssets/MyAssets/AssetView.swift
@@ -14,6 +14,8 @@ struct AssetView: View {
                 VStack(spacing: 30) {
                     Spacer()
                     AssetMenuGridView()
+                    AssetBannerView()
+                        .aspectRatio(5/2, contentMode: .fit)
                 }
             }
             .background(Color.gray.opacity(0.2))
