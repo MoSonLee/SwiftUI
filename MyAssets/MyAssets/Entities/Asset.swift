643e1564689de557771e99f3702426ef3fd94fa4
이승후 <ron222@naver.com>
2022-03-25 20:56:46 +0900

    Update: Asset class

--- MyAssets/MyAssets/Entities/Asset.swift
+++ MyAssets/MyAssets/Entities/Asset.swift
@@ -22,10 +22,65 @@ class AssetData: Identifiable, ObservableObject, Decodable {
     let id: Int
     let title: String
     let amount: String
+    let creditCardAmounts: [CreditCardAmounts]?
     
-    init(id: Int, title: String, amount: String) {
+    init(id: Int, title: String, amount: String, creditCardAmounts: [CreditCardAmounts]? = nil) {
         self.id = id
         self.title = title
         self.amount = amount
+        self.creditCardAmounts = creditCardAmounts
+    }
+}
+
+enum CreditCardAmounts: Identifiable, Decodable {
+    case previousMonth(amount: String)
+    case currentMonth(amount: String)
+    case nextMonth(amount: String)
+    
+    var id: Int {
+        switch self {
+        case .previousMonth:
+            return 0
+        case .currentMonth:
+            return 1
+        case .nextMonth:
+            return 2
+        }
+    }
+    
+    var amount: String {
+        switch self {
+        case .previousMonth(let amount),
+                .currentMonth(let amount),
+                .nextMonth(let amount):
+            return amount
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case previousMonth
+        case currentMonth
+        case nextMonth
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let value = try? values.decode(String.self, forKey: .previousMonth) {
+            self = .previousMonth(amount: value)
+            return
+        }
+        
+        if let value = try? values.decode(String.self, forKey: .currentMonth) {
+            self = .currentMonth(amount: value)
+            return
+        }
+        
+        if let value = try? values.decode(String.self, forKey: .nextMonth) {
+            self = .nextMonth(amount: value)
+            return
+        }
+        
+        throw fatalError("ERROR: CreditCardAmouts JSon Decoding")
     }
 }
