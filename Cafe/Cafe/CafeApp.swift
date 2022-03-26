7c8a02ccb3a72786af830f92b372b8a4a1268761
이승후 <ron222@naver.com>
2022-03-26 20:29:55 +0900

    Add: Cafe app

---
+++ Cafe/Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by 이승후 on 2022/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
