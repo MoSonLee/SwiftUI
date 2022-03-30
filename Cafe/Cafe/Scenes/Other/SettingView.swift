5e11cf1f73195b3c5d848f24f270fa64014299bc
이승후 <ron222@naver.com>
2022-03-31 01:47:50 +0900

    Add: SettingVIew

---
+++ Cafe/Cafe/Scenes/Other/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  Cafe
+//
+//  Created by 이승후 on 2022/03/31.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
