f2001b09392ef81ac6a6126f930ae1fa0c5d5a2e
이승후 <ron222@naver.com>
2022-04-04 11:26:20 +0900

    Add: RandomPeople app(alamofire, URLimage package)

---
+++ RandomPeople/RandomPeople/RandomPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPeopleApp.swift
+//  RandomPeople
+//
+//  Created by 이승후 on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct RandomPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
