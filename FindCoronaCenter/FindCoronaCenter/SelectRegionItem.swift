60327b08d9d471f0ce43be13638dd38ba369fa98
이승후 <ron222@naver.com>
2022-04-04 22:00:23 +0900

    Feat: SelectRegionItem 구현 완료

--- FindCoronaCenter/FindCoronaCenter/SelectRegionItem.swift
+++ FindCoronaCenter/FindCoronaCenter/SelectRegionItem.swift
@@ -4,5 +4,33 @@
 //
 //  Created by 이승후 on 2022/04/04.
 //
+import SwiftUI
 
-import Foundation
+struct SelectRegionItem: View {
+    var region: Center.Sido
+    var count: Int
+    var body: some View {
+        ZStack {
+            Color(white: 0.9)
+            VStack {
+                Text(region.rawValue)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.purple)
+                Text("(\(count))")
+                    .font(.callout)
+                    .fontWeight(.light)
+                    .foregroundColor(.gray)
+            }
+            .padding()
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct SelectRegionItem_Previews: PreviewProvider {
+    static var previews: some View {
+        let region0 = Center.Sido.경상남도
+        SelectRegionItem(region: region0, count: 3)
+    }
+}
