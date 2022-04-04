9600e71f820557fea579f97502fe00fc928c419b
이승후 <ron222@naver.com>
2022-04-04 21:49:51 +0900

    Feat: SelectRegionViewModel 구현

--- FindCoronaCenter/FindCoronaCenter/SelectRegionViewModel.swift
+++ FindCoronaCenter/FindCoronaCenter/SelectRegionViewModel.swift
@@ -9,5 +9,21 @@ import Foundation
 import Combine
 
 class SelectRegionViewModel: ObservableObject {
+    @Published var centers = [Center.Sido: [Center]]()
+    private var cancellables = Set<AnyCancellable>()
     
+    init(centerNetwork: CenterNetwork = CenterNetwork()){
+        centerNetwork.getCenterList()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: {[weak self] in
+                guard case .failure(let error) = $0 else { return }
+                print(error.localizedDescription)
+                self?.centers = [Center.Sido: [Center]]()
+            },
+                  receiveValue: { [weak self] centers in
+                self?.centers = Dictionary(grouping: centers) { $0.sido }
+            }
+            )
+            .store(in: &cancellables)
+    }
 }
