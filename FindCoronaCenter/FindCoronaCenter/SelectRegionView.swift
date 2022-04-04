6210e14773b8c1f9ae96637ce589ca2f104c6024
이승후 <ron222@naver.com>
2022-04-04 21:36:03 +0900

    Add: SelectRegionView, ViewModel

---
+++ FindCoronaCenter/FindCoronaCenter/SelectRegionView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectRegionView.swift
+//  FindCoronaCenter
+//
+//  Created by 이승후 on 2022/04/04.
+//
+
+import SwiftUI
+
+struct SelectRegionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectRegionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectRegionView()
+    }
+}
---
+++ FindCoronaCenter/FindCoronaCenter/SelectRegionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SelectRegionViewModel.swift
+//  FindCoronaCenter
+//
+//  Created by 이승후 on 2022/04/04.
+//
+
+import Foundation
+import Combine
+
+class SelectRegionViewModel: ObservableObject {
+    
+}
