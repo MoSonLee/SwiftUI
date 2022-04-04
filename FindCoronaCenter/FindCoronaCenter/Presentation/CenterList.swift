b18bb08371869fbf1de5cfb11d3a7f7744c0c7c7
이승후 <ron222@naver.com>
2022-04-04 22:25:08 +0900

    Add: CenterList

---
+++ FindCoronaCenter/FindCoronaCenter/Presentation/CenterList.swift
@@ -0,0 +1,20 @@
+//
+//  CenterList.swift
+//  FindCoronaCenter
+//
+//  Created by 이승후 on 2022/04/04.
+//
+
+import SwiftUI
+
+struct CenterList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CenterList_Previews: PreviewProvider {
+    static var previews: some View {
+        CenterList()
+    }
+}
