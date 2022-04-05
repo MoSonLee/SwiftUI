4afadebe694912f0d8fce04c734a144bc0b87868
이승후 <ron222@naver.com>
2022-04-05 21:32:07 +0900

    Add: Memo app + coredata, memory group

---
+++ swiftUI_memoApp/swiftUI_memoApp/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  swiftUI_memoApp
+//
+//  Created by 이승후 on 2022/04/05.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
---
+++ swiftUI_memoApp/swiftUI_memoApp/swiftUI_memoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  swiftUI_memoAppApp.swift
+//  swiftUI_memoApp
+//
+//  Created by 이승후 on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_memoAppApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            MainListView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
