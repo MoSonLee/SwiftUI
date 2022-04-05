6641aa7a255d95e1b00362def086431fd614faff
이승후 <ron222@naver.com>
2022-04-06 00:33:55 +0900

    Feat: ComposeView 구현 완료

---
+++ swiftUI_memoApp/swiftUI_memoApp/View/ComposeView.swift
@@ -0,0 +1,51 @@
+//
+//  ComposeView.swift
+//  swiftUI_memoApp
+//
+//  Created by 이승후 on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    @EnvironmentObject var store: MemoStore
+    @State private var content: String = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $content)
+                    .padding()
+            }
+            .navigationTitle("new Memo")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading)
+                {
+                    Button{
+                        dismiss()
+                    } label: {
+                        Text("취소")
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing)
+                {
+                    Button{
+                        store.insert(memo: content)
+                        dismiss()
+                    } label: {
+                        Text("저장")
+                    }
+                }
+            }
+        }
+    }
+}
+struct ComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeView()
+            .environmentObject(MemoStore())
+    }
+}
+
